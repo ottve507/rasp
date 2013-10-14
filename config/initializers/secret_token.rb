0bb34c457879d733d65db4888ede25aa9425a5d0
Otto Velander <o.velander@gmail.com>
2013-10-14 21:04:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rasp::Application.config.secret_token = '111ca3ce97ff61a206689caa314d86089fb6f0818f0780f3fe9caeaf807ed98ab81c36f6a4287845e987c700a6a2b0a7396b84704ee9ae133b274e4172fc4f4c'
---
+++ db/migrate/20131014185433_create_buttons.rb
@@ -0,0 +1,10 @@
+class CreateButtons < ActiveRecord::Migration
+  def change
+    create_table :buttons do |t|
+      t.string :name
+      t.boolean :toggle
+
+      t.timestamps
+    end
+  end
+end
